8653ea5650690aa3cb81d7124ed26d4b4fa3ded5
Rafael Grossi <rg.grossi@outlook.com>
2024-03-09 00:53:23 -0300

    guessing game, lesson 2

--- projects/guessing_game/target/debug/deps/guessing_game-c0baf58ba2e8e2b6.d
+++
@@ -1,5 +0,0 @@
-/home/drops/Programming/Rust/projects/guessing_game/target/debug/deps/guessing_game-c0baf58ba2e8e2b6: src/main.rs
-
-/home/drops/Programming/Rust/projects/guessing_game/target/debug/deps/guessing_game-c0baf58ba2e8e2b6.d: src/main.rs
-
-src/main.rs:
--- projects/guessing_game/target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-/home/drops/Programming/Rust/projects/guessing_game/target/debug/guessing_game: /home/drops/Programming/Rust/projects/guessing_game/src/main.rs
